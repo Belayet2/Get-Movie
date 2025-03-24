7d2face55b8a2860f29006ec3a11f2cbb22871fa
Belayet Hossain <belayet.bel.22@gmail.com>
2025-03-24 16:53:50 +0600

    Error Fix

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,12 +1,10 @@
 // app/thank-you/page.tsx
 "use client";
 
-import { useSearchParams, useRouter } from "next/navigation";
+import {useRouter } from "next/navigation";
 
 export default function ThankYouPage() {
-  const searchParams = useSearchParams();
   const router = useRouter();
-  const movieTitle = searchParams.get("movie");
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900">
@@ -15,7 +13,7 @@ export default function ThankYouPage() {
           Thank You!
         </h1>
         <p className="text-gray-600 dark:text-gray-300">
-          We've received your recommendation for "{movieTitle}" and added it to our pending list.
+          We've received your recommendation.
         </p>
         <p className="text-gray-600 dark:text-gray-300 mt-4 mb-6 font-bold">
           Our team will add your recommended movie under 12 hours.
